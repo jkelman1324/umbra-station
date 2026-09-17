d69df174f4bcff47e2227c946d3d859d4c142575
Jake Kelman <j.d.kelman95@gmail.com>
2026-09-17 07:12:49 -0700

    Support for missing icon file.

--- script.py
+++ script.py
@@ -2,8 +2,9 @@ import sys
 import os
 import requests
 import json
-from datetime import datetime
 
+from datetime import datetime
+from time import sleep
 from waveshare_epd import epd7in5b_V2
 from PIL import Image, ImageDraw, ImageFont, ImageOps
 
@@ -16,13 +17,18 @@ icons_path = os.path.join(project_dir, 'assets', 'weather')
 
 WEATHER_ICONS = {
     "day": {
-        "Cloudy": "cloud.fill@3x.png",
-        "Mostly Sunny": "sun.max.fill@3x.png",
-        "Partly Sunny": "cloud.sun.fill@3x.png",
-        "Sunny": "sun.max.fill@3x.png",
+        "Cloudy": "cloud@3x.png",
+        "Mostly Cloudy": "cloud@3x.png",
+        "Mostly Sunny": "sun.max@3x.png",
+        "Partly Sunny": "cloud.sun@3x.png",
+        "Patchy Drizzle": "cloud.drizzle@3x.png",
+        "Sunny": "sun.max@3x.png",
     },
     "night": {
+        "Mostly Clear": "moon@3x.png",
+        "Mostly Cloudy": "cloud@3x.png",
         "Partly Cloudy": "cloud.moon@3x.png",
+        "Patchy Drizzle": "cloud.drizzle@3x.png",
     },
 }
 
@@ -47,6 +53,9 @@ def get_coordinates():
 def get_weather_icon(condition, isDaytime) -> Image.Image:
     daytime = "day" if isDaytime else "night"
     filename = WEATHER_ICONS[daytime].get(condition)
+    if filename is None:
+        filename = "questionmark@3x.png"
+
     return Image.open(f"{icons_path}/{filename}")
 
 def draw_current_weather(image, draw_image, current_period):
@@ -73,7 +82,7 @@ def draw_current_weather(image, draw_image, current_period):
     icon_size = 96
     icon_bg = Image.new("L", icon.size, 255)
     icon_bg.paste(0, mask=icon.getchannel("A"))
-    image.paste(icon_bg, (int(mid_x - icon_size / 2), 200))
+    image.paste(icon_bg, (int(mid_x - icon_bg.width / 2), 200))
 
     # Temp
     temp = f"{current_period["temperature"]}°"
@@ -123,35 +132,38 @@ def main():
 
     lat, lon = get_coordinates()
 
-    try:
-        response = requests.get(f"https://api.weather.gov/points/{lat},{lon}")
-        data = response.json()
-        city = data['properties']['relativeLocation']['properties']['city']
-        url = data['properties']['forecastHourly']
-        response = requests.get(url)
-        data = response.json()
-        periods = data['properties']['periods'][:5]
-        print(json.dumps(periods, indent = 4))
-    except Exception as e:
-        print(f"Error fetching weather data: {e}")
+    while True:
+        try:
+            response = requests.get(f"https://api.weather.gov/points/{lat},{lon}")
+            data = response.json()
+            city = data['properties']['relativeLocation']['properties']['city']
+            url = data['properties']['forecastHourly']
+            response = requests.get(url)
+            data = response.json()
+            periods = data['properties']['periods'][:5]
+            print(json.dumps(periods, indent = 4))
+        except Exception as e:
+            print(f"Error fetching weather data: {e}")
 
-    image = Image.new('1', (epd.width, epd.height), 255)  # 255: clear the frame
-    red_image = Image.new('1', (epd.width, epd.height), 255)  # 255: clear the frame
-    draw_image = ImageDraw.Draw(image)
-    draw_red = ImageDraw.Draw(red_image)
+        image = Image.new('1', (epd.width, epd.height), 255)  # 255: clear the frame
+        red_image = Image.new('1', (epd.width, epd.height), 255)  # 255: clear the frame
+        draw_image = ImageDraw.Draw(image)
+        draw_red = ImageDraw.Draw(red_image)
 
-    # Title
-    title = f"{city} Forecast"
-    draw_image.text((10, 10), title, font = bold48, fill = 0)
-    line_end_x = 10 + draw_image.textlength(title, font = bold48)
-    draw_image.line((10, 60, line_end_x, 60), fill = 0)
+        # Title
+        title = f"{city} Forecast"
+        draw_image.text((10, 10), title, font = bold48, fill = 0)
+        line_end_x = 10 + draw_image.textlength(title, font = bold48)
+        draw_image.line((10, 60, line_end_x, 60), fill = 0)
 
-    draw_current_weather(image, draw_image, periods[0])
+        draw_current_weather(image, draw_image, periods[0])
 
-    for i in range(1, 5):
-        draw_forecasted_weather(image, draw_image, i, periods[i])
+        for i in range(1, 5):
+            draw_forecasted_weather(image, draw_image, i, periods[i])
 
-    epd.display(epd.getbuffer(image), epd.getbuffer(red_image))
+        epd.display(epd.getbuffer(image), epd.getbuffer(red_image))
+        sleep(60 * 60)
+        print("Sleep done.")
 
     # epd.sleep()
 
